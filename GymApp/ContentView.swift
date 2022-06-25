9c423fc27b2db6df0f1e854c9e2a7c4513c537ad
Shuhei Koyanagi <kobry24nosk@icloud.com>
2022-06-25 21:58:38 +0900

    Initial Commit

---
+++ GymApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GymApp
+//
+//  Created by Shuhei Koyanagi on 2022/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GymApp/GymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppApp.swift
+//  GymApp
+//
+//  Created by Shuhei Koyanagi on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct GymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
